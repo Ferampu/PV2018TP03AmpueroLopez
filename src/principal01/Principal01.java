96c9278d5ab121e829ad516468de7b94e3fe4b2c
acer <acer@LAPTOP-1O4L23R3>
2018-04-29 22:19:53 -0300

    Punto1

--- src/principal01/Principal01.java
+++ src/principal01/Principal01.java
@@ -15,10 +15,15 @@ public class Principal01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        iniciarPrograma();
         // TODO code application logic here
     }
     public static void iniciarPrograma(){
-        
+        Punto1 unArreglo=new Punto1();
+        unArreglo.cargar();
+        System.out.println("El numero mayor del arreglo es: "+unArreglo.mostrarMayor());
+        System.out.println("El numero menor del arreglo es: "+unArreglo.mostrarMenor());
+        System.out.println("El promedio es: "+unArreglo.calcularProm());
     }
     
 }
